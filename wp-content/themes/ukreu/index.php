fb308b789518e7e6b451d71e40d58af55ec5d791
Viktor Soshko <Lorgar4@ukr.net>
2017-09-17 16:06:27 +0300

    add content in index

--- wp-content/themes/ukreu/index.php
+++ wp-content/themes/ukreu/index.php
@@ -1,10 +1,61 @@
 
 <?php get_header(); ?>
                 <div class="container-fluid">
-                    <div class="row">
+                    <div class="row eq">
                         <div class="col-md-3 col-sm-3 aside">
                            <?php get_sidebar();?>
                         </div>
+                        <div class="col-md-9 col-sm-9">
+                            <div class="HP-wrap">
+                            <div class="HP-slide">
+
+                            </div>
+                            <div class="row">
+                                <div class="col-md-7 col-sm-7">
+                                    <div class="HP-info">*Вартість  за  перший  рік,  далі  п’ять  євро  на  рік.  Для громадських
+                                        організацій та державних установ вартість незмінна - три євро на рiк.
+                                    </div>
+                                </div>
+                                <div class="col-md-5 col-sm-5">
+                                    <div class="HP-info">Увага,  вартість  послуги  електронної  пошти
+                                        складає 1 євро на рік. Місце на сервері 1,5 Гб.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="HP-advanteges-wrap">
+                                    <div class="col-md-4 col-sm-4">
+                                        <div class="HP-advanteges">
+                                            <img src="<?php echo bloginfo('template_url')?>/img/advanteges1.png" alt="img">
+                                            <h3>Доменні імена</h3>
+                                            <p>Більше 10 років ми забезпечуємо якісну роботу великої кількості різноманітних сайтів.</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 col-sm-4">
+                                        <div class="HP-advanteges">
+                                            <img src="<?php echo bloginfo('template_url')?>/img/advanteges2.png" alt="img">
+                                            <h3>Бізнес електронна пошта</h3>
+                                            <p>Більше 10 років ми забезпечуємо якісну роботу великої кількості різноманітних сайтів.</p>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 col-sm-4">
+                                        <div class="HP-advanteges">
+                                            <img src="<?php echo bloginfo('template_url')?>/img/advanteges3.png" alt="img">
+                                            <h3>Хостинг</h3>
+                                            <p>Більше 10 років ми забезпечуємо якісну роботу великої кількості різноманітних сайтів.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12 col-sm-12">
+                                    <div class="HP-bottom-info">
+                                        <p>Відразу після реєстрації домену, через будь-якого реєстратора, Ваші особисті дані потрапляють в мережу. Вони стають доступні кожному користувачеві Інтернету. Хакери і спамери можуть використовувати цю інформацію проти Вас і Вашої родини. За допомогою спеціального сервісу ми приховуємо цю інформацію від загального перегляду, замінюючи її на Private Protection. При цьому Ви залишаєтеся власником домену і повністю його контролюєте.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                     </div>
                     </div>
                 </div>
             </div>
